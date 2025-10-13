086f714da6935a595b29611a8f6d2ad0f3fc01f4
minsu6862 <chl6863@gmail.com>
2025-10-13 11:20:57 +0900

    리엑트 초기 설정

---
+++ src/component/Navbar.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo">minsu board project</div>
+            <div className="menu">
+                <Link to="/">Home</Link>
+                <Link to="/board">게시판</Link>
+                <Link to="/login">로그인</Link>
+                <Link to="/signup">회원가입</Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
